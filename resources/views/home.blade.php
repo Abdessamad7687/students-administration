2d0bb54f5a6b4c763b777431ae8f9eaf031eaf46
Abdessamad7687 <abdessamadahmali@gmail.com>
2023-02-15 20:29:43 +0100

    V1.0

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,11 +7,11 @@
                     <div class="card-body">
                         <p class="card-title text-left">Full-Name: <strong>{{ $Student['name'] }}</strong></p>
                         <hr>
-                        <p class="card-text ">Mathematics: <strong></strong> </p>
-                        <p class="card-text">Physics: <strong></strong> </p>
-                        <p class="card-text">Science: <strong></strong> </p>
-                        <p class="card-text">English: <strong></strong> </p>
-                        <p class="card-text">French: <strong></strong> </p>
+                        <p class="card-text ">Mathematics: <strong>{{ $Student['mathematics'] }}</strong> </p>
+                        <p class="card-text">Physics: <strong>{{ $Student['physics'] }}</strong> </p>
+                        <p class="card-text">Science: <strong>{{ $Student['science'] }}</strong> </p>
+                        <p class="card-text">English: <strong>{{ $Student['english'] }}</strong> </p>
+                        <p class="card-text">French: <strong>{{ $Student['french'] }}</strong> </p>
                     </div>
             </div>
         @endforeach
