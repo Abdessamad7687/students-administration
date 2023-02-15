f592aadb4c88a8dbd41fe09aaedc83fa7abd09a0
Abdessamad7687 <abdessamadahmali@gmail.com>
2023-02-15 19:57:27 +0100

    V1.0

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,17 @@
 
 @section('content')
 
-
+<div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title text-center">Full-Name:  </h5>
+                    <hr>
+                    <p class="card-subtitle mb-2">Mathematics:  </p>
+                    <p class="card-text">Physics:  </p>
+                    <p class="card-text">Science:  </p>
+                    <p class="card-text">English:  </p>
+                    <p class="card-text">French: </p>
+                    <p class="card-text">Geographic:  </p>
+                </div>
+            </div>
 
 @endsection
